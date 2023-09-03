eeb7bddb46290627b590b6d7b2eaf1b9362c7237
Kajal Kumari <114283323+kajalkumari2507@users.noreply.github.com>
2023-09-03 19:47:39 +0530

    Add files via upload

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import React from "react";
 const Loader = () => {
   return (
     <div className="loader">
-      <div className="loader-spinner"></div>
+      <div className="spinner"></div>
     </div>
   );
 };
